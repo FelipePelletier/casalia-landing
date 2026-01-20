223daa56f47be8908a30812c6594331346e0c970
Felipe Pelletier <felipelletier@gmail.com>
2026-01-20 13:29:56 -0300

    se quito el cursor animado del hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -71,11 +71,11 @@ export function Hero() {
         </div>
       </div>
 
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
+      {/* <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
         <div className="w-6 h-10 rounded-full border-2 border-card/60 flex items-start justify-center p-2">
           <div className="w-1 h-2 bg-card/60 rounded-full" />
         </div>
-      </div>
+      </div> */}
     </section>
   );
 }
